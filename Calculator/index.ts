7f5373035c65844a8d6632a96ca209e86e313d54
Muhammadzainattiq <zainatteeq@gmail.com>
2024-01-19 21:56:38 +0500

    Calculator main code added

---
+++ Calculator/index.ts
@@ -0,0 +1,58 @@
+import inquirer, {Answers,QuestionCollection} from "inquirer";
+import chalk from 'chalk'
+// // Here is the calculator
+console.log(chalk.bold.bgBlue.white("ZAIN'S CALCULATOR IS HERE :"));
+
+let questions:QuestionCollection= [
+    {
+        name : 'num1',
+        type : 'number',
+        message : "enter the no 1 : "
+
+    }
+    , {
+        name : 'num2',
+        type : 'number',
+        message : "enter the no 2 : "
+    }
+    ,{
+        name : 'operators',
+        type : 'list',
+        choices : ["+","-","*","/","**","%"],
+        message : "Enter the operation you want to perform : " 
+    }
+]
+let answers : Promise<Answers> = inquirer.prompt(questions)
+answers.then((answer:Answers)=>{
+    switch (answer.operators) {
+        case "+" :
+            console.log(chalk.blue.bgWhite.bold.underline(`${answer.num1} + ${answer.num2} = ${answer.num1 + answer.num2} Answer.`));
+
+            break;
+        case "-" :
+            console.log(chalk.green.bgWhite.bold.underline(`${answer.num1} - ${answer.num2} = ${answer.num1 - answer.num2} Answer.`));
+
+            break;
+        case "*" :
+            console.log(chalk.yellow.bgWhite.bold.underline(`${answer.num1} * ${answer.num2} = ${answer.num1 * answer.num2} Answer.`));
+
+            break;
+        case "%" :
+            console.log(chalk.grey.bgWhite.bold.underline(`${answer.num1} % ${answer.num2} = The remainder will be : ${answer.num1 % answer.num2} Answer.`))
+
+            break;
+        case "**" :
+            console.log(chalk.black.bgWhite.bold.underline(`${answer.num1} ** ${answer.num2} = ${answer.num1 ** answer.num2} Answer.`));
+
+            break;
+
+ 
+        default:
+            break;
+    }
+})
+answers.catch((error)=>{
+    console.log("Error", error);
+
+})
+
