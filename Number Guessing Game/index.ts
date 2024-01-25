454ace53db51aca358f5b7254252226623deec4f
Muhammadzainattiq <zainatteeq@gmail.com>
2024-01-25 21:59:36 +0500

    updated

--- Number Guessing Game/index.ts
+++ Number Guessing Game/index.ts
@@ -1,13 +1,17 @@
 import inquirer from "inquirer";
 import chalk from 'chalk';
+
 console.log(chalk.bold.underline.red("Welcome To the Number Guessing game"));
-let randomNumber = Math.floor(Math.random() * 100);
+let randomNumber:number = Math.floor(Math.random()*100)
+
 const playerName = await inquirer.prompt({
     name: "name",
     type: "input",
     message: "Enter your name here: "
 });
+
 console.log(chalk.blueBright(`Welcome my dear ${playerName.name} to the Number Guessing Game`));
+
 const gameFunc = async () => {
     let readyToPlay = await inquirer.prompt({
         name: "ready",
@@ -15,33 +19,37 @@ const gameFunc = async () => {
         message: "Are you ready to play:",
         choices: ["Yes", "No"]
     });
+
     if (readyToPlay.ready === "Yes") {
-        let score = 100;
-        let gn = -1;
+        let score: number = 100;
+        let gn: number = -1;
+
         while (gn !== randomNumber) {
             let guessedNumber = await inquirer.prompt({
                 name: "guessedNumber",
                 type: 'number',
                 message: chalk.bgWhite("Guess the number (1-100) (Max 20 attempts): ")
             });
+
             gn = guessedNumber.guessedNumber;
+
             if (guessedNumber.guessedNumber > randomNumber) {
                 console.log(chalk.bgRed("Sorry, The guessed number is greater than the actual number"));
                 score -= 5;
-            }
-            else if (guessedNumber.guessedNumber < randomNumber) {
+            } else if (guessedNumber.guessedNumber < randomNumber) {
                 console.log(chalk.bgRed("Sorry, The guessed number is smaller than the actual number"));
                 score -= 5;
             }
+
             if (guessedNumber.guessedNumber === randomNumber) {
                 console.log(chalk.bold.underline.italic.bgGreen.black("So Smart! You predicted right. ", randomNumber));
                 console.log(chalk.bold.underline.italic.bgBlue.black("Your score is:", score, ' out of 100'));
                 break;
             }
         }
-    }
-    else {
+    } else {
         console.log(chalk.green("Allah Hafiz! See you next time"));
     }
 };
+
 gameFunc();
